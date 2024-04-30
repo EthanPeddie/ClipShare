1f0c47e4de82c7f961f1dae002b1e2af90d54614
EthanPeddie <kaungmyatthu2620@gmail.com>
2024-05-01 00:33:30 +0630

    Remove S3

--- src/app/utils/S3Bucket.ts
+++
@@ -1,7 +0,0 @@
-import {S3} from 'aws-sdk';
-
-export const S3Bucket = new S3({
-  accessKeyId: 'AKIA47CRVELAMUNPUFTR',
-  secretAccessKey: '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',
-  region: 'ap-southeast-1',
-});
